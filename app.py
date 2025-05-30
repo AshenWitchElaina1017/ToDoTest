e7e16a90430545c0e057ae7b181ecae4f47da4f4
Elaina <144452824+AshenWitchElaina1017@users.noreply.github.com>
2025-05-31 00:15:43 +0800

    Initial commit of ToDoDemo project

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo = request.form.get('todo')
+    if todo:
+        todos.append({'task': todo, 'done': False})
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:todo_id>')
+def complete_todo(todo_id):
+    if 0 <= todo_id < len(todos):
+        todos[todo_id]['done'] = not todos[todo_id]['done']
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    if 0 <= todo_id < len(todos):
+        todos.pop(todo_id)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,2 @@
+// No specific JavaScript functionality for this simple ToDo app yet.
+// All interactions are handled by Flask routes.
